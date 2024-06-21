57496939f92a65666d8cc2513f6772f9c4da248c
Rana Shazib Ali <shazib.24sr@gmail.com>
2024-06-21 16:14:27 +0500

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -64,7 +64,7 @@ const Hero = () => {
                         <motion.a href="#Contact" variants={textVariants}>
   <motion.button>Contact Me</motion.button>
 </motion.a>
-<motion.a href="https://drive.google.com/file/d/1kXwJT9J2TQrw_2TeeKNziqYZUyK3ltq-/view?usp=drive_link" variants={textVariants} target="_blank" rel="noopener noreferrer">
+<motion.a href="https://drive.google.com/file/d/1ONmwfGrs7LJayItwAGys-wllpw66L7r0/view?usp=drive_link" variants={textVariants} target="_blank" rel="noopener noreferrer">
   <motion.button>Download Resume</motion.button>
 </motion.a>
 
